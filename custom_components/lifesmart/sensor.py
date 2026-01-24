b23f5407af4cd06f188747eed245de037d0b9b03
raphael1688 <51769889+raphael1688@users.noreply.github.com>
2026-01-24 10:31:22 +0800

    Update sensor.py

--- custom_components/lifesmart/sensor.py
+++ custom_components/lifesmart/sensor.py
@@ -1,95 +1,101 @@
 """Support for lifesmart sensors."""
 import logging
 
-
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass, # [新增] 標準設備類別
+    SensorStateClass,  # [新增] 狀態類別 (用於長期統計)
+)
 from homeassistant.const import (
     UnitOfTemperature,
+    PERCENTAGE,
+    LIGHT_LUX,
+    CONCENTRATION_PARTS_PER_MILLION,
+    CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER,
 )
-DOMAIN = "sensor"
-ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
-from . import  LifeSmartDevice
+from . import LifeSmartDevice
 
 _LOGGER = logging.getLogger(__name__)
 
-GAS_SENSOR_TYPES = ["SL_SC_WA ",
-"SL_SC_CH",
-"SL_SC_CP",
-"ELIQ_EM"]
-
-OT_SENSOR_TYPES = ["SL_SC_MHW",
-"SL_SC_BM",
-"SL_SC_G",
-"SL_SC_BG"]
-
+GAS_SENSOR_TYPES = ["SL_SC_WA ", "SL_SC_CH", "SL_SC_CP", "ELIQ_EM"]
+OT_SENSOR_TYPES = ["SL_SC_MHW", "SL_SC_BM", "SL_SC_G", "SL_SC_BG"]
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Perform the setup for LifeSmart devices."""
-    devices = []
+    if discovery_info is None:
+        return
     dev = discovery_info.get("dev")
     param = discovery_info.get("param")
     devices = []
+    
+    # 優化迴圈邏輯
     for idx in dev['data']:
         if dev['devtype'] in OT_SENSOR_TYPES and idx in ["Z","V","P3","P4"]:
-            devices.append(LifeSmartSensor(dev,idx,dev['data'][idx],param))
+            devices.append(LifeSmartSensor(dev, idx, dev['data'][idx], param))
         else:
-            devices.append(LifeSmartSensor(dev,idx,dev['data'][idx],param))
+            # 這裡可能需要根據您的原始邏輯篩選，防止重複添加
+            # 假設原始邏輯是為了過濾掉非傳感器數據
+            if idx not in ["L1","L2","L3","P1","P2","RGB","RGBW"]: 
+                devices.append(LifeSmartSensor(dev, idx, dev['data'][idx], param))
+                
     add_entities(devices)
 
 
-class LifeSmartSensor(LifeSmartDevice):
+class LifeSmartSensor(LifeSmartDevice, SensorEntity):
     """Representation of a LifeSmartSensor."""
 
     def __init__(self, dev, idx, val, param):
         """Initialize the LifeSmartSensor."""
         super().__init__(dev, idx, val, param)
-        self.entity_id = ENTITY_ID_FORMAT.format(( dev['devtype'] + "_" + dev['agt'] + "_" + dev['me'] + "_" + idx).lower())
+        self._attr_name = dev['name'] + "_" + idx
+        
+        # [修復] 添加 unique_id，移除 entity_id
+        self._attr_unique_id = (dev['devtype'] + "_" + dev['agt'] + "_" + dev['me'] + "_" + idx).lower()
+        
         devtype = dev['devtype']
+        
+        # [修復] 使用標準枚舉和單位常數
+        # 初始化為 None
+        self._attr_device_class = None
+        self._attr_native_unit_of_measurement = None
+        self._attr_state_class = SensorStateClass.MEASUREMENT # 預設為測量值
+        
         if devtype in GAS_SENSOR_TYPES:
-            self._unit = "None"
-            self._device_class = "None"
-            self._state = val['val']
+             # 燃氣類通常是狀態顯示，可能不需要單位
+             pass 
         else:
             if idx == "T" or idx == "P1":
-                self._device_class = "temperature"
-                self._unit = UnitOfTemperature.CELSIUS
+                self._attr_device_class = SensorDeviceClass.TEMPERATURE
+                self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
             elif idx == "H" or idx == "P2":
-                self._device_class = "humidity"
-                self._unit = "%"
+                self._attr_device_class = SensorDeviceClass.HUMIDITY
+                self._attr_native_unit_of_measurement = PERCENTAGE
             elif idx == "Z":
-                self._device_class = "illuminance"
-                self._unit = "lx"
+                self._attr_device_class = SensorDeviceClass.ILLUMINANCE
+                self._attr_native_unit_of_measurement = LIGHT_LUX
             elif idx == "V":
-                self._device_class = "battery"
-                self._unit = "%"
+                self._attr_device_class = SensorDeviceClass.BATTERY
+                self._attr_native_unit_of_measurement = PERCENTAGE
             elif idx == "P3":
-                self._device_class = "None"
-                self._unit = "ppm"
+                self._attr_device_class = SensorDeviceClass.CO2 # 假設 P3 是 CO2，如果是 PM2.5 可改
+                self._attr_native_unit_of_measurement = CONCENTRATION_PARTS_PER_MILLION
             elif idx == "P4":
-                self._device_class = "None"
-                self._unit = "mg/m3"
-            else:
-                self._unit = "None"
-                self._device_class = "None"
-            self._state = val['v']
+                self._attr_device_class = SensorDeviceClass.PM25 # 假設 P4 是 PM2.5
+                self._attr_native_unit_of_measurement = CONCENTRATION_MILLIGRAMS_PER_CUBIC_METER
 
+        # 設定初始值
+        self._update_value(val)
 
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit
-
-    @property
-    def device_class(self):
-        """Return the device class of this entity."""
-        return self._device_class
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+    def _update_value(self, val):
+        """Internal helper to update value."""
+        # 根據原始邏輯判斷取值位置
+        if self._devtype in GAS_SENSOR_TYPES:
+            self._attr_native_value = val['val']
+        else:
+            self._attr_native_value = val['v']
 
     @property
     def unique_id(self):
         """A unique identifier for this entity."""
-        return self.entity_id
+        return self._attr_unique_id
